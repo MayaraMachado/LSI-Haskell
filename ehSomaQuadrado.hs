609ea9169394108e362e16677c95a50edaaefc9b
Mayara Machado <maaih18@gmail.com>
2017-12-08 12:51:52 -0200

    É isso - Atualização com inserção da feature leitura de entrada

--- ehSomaQuadrado.hs
+++ ehSomaQuadrado.hs
@@ -6,8 +6,7 @@ ehSomaQuadrado sqrtNum rt num
       | (sqrtNum ** 2) + (rt ** 2) > num  && sqrtNum == (sqrt(num)-1) = "NO"
       | (sqrtNum ** 2) + (rt ** 2) == num = "YES"
       | otherwise = ehSomaQuadrado (sqrtNum-1) (rt+1) num
-      
-main :: IO ()     
+       
 main = do 
     n <- getLine
     putStrLn(ehSomaQuadrado (sqrt (read n)) 0 (read n :: Float))
